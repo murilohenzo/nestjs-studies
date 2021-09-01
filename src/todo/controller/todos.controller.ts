6c12ba01e063f35ae91ce5b7dfedeabb2193dbab
murilohenzo <murilohezo@gmail.com>
2021-09-01 10:06:16 -0300

    [feat]:add/ unit test and swagger

--- src/todo/controller/todos.controller.ts
+++ src/todo/controller/todos.controller.ts
@@ -1,20 +1,50 @@
 import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateTodoDto } from '../dto/create-todo.dto';
 import { Todo } from '../interface/todo.interface';
 import { TodoService } from '../service/create-todo.service';
 
+@ApiTags('/api/v1/todos')
 @Controller('/api/v1/todos')
 export class TodoController {
   constructor(private readonly _todoService: TodoService) {}
 
   @Post()
   @HttpCode(201)
+  @ApiOperation({
+    summary: 'Create todo.',
+  })
+  @ApiOkResponse({
+    description: 'Todo created successfully.',
+    type: CreateTodoDto,
+  })
+  @ApiNotFoundResponse({ description: 'Todo already exists.' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
   async create(@Body() todo: CreateTodoDto): Promise<Todo> {
     return await this._todoService.create(todo);
   }
 
   @Get()
   @HttpCode(200)
+  @ApiOperation({
+    summary: 'Get all todos',
+  })
+  @ApiOkResponse({
+    description: 'Retrieve list of todos successfully',
+    type: [CreateTodoDto],
+  })
+  @ApiNotFoundResponse({ description: 'Not exists todos.' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
   async findAll(): Promise<Todo[]> {
     return await this._todoService.findAll();
   }
--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,5 +1,15 @@
 /* eslint-disable prettier/prettier */
+
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateTodoDto {
+  @ApiProperty({
+    description: "The title of todo.",
+  })
   title: string;
+
+  @ApiProperty({
+    description: "The status of the todo.",
+    default: false,
+  })
   done: boolean;
 }
